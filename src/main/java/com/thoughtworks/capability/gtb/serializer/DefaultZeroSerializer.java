8825461ef9a8cb0dd519685b5944bf0a8a15292d
flappyBug <tdkgn@outlook.com>
2020-11-07 16:59:22 +0800

    serde PersonVo

---
+++ src/main/java/com/thoughtworks/capability/gtb/serializer/DefaultZeroSerializer.java
@@ -0,0 +1,20 @@
+package com.thoughtworks.capability.gtb.serializer;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
+import java.io.IOException;
+import java.util.Optional;
+
+public class DefaultZeroSerializer extends StdSerializer<Integer> {
+
+    protected DefaultZeroSerializer() {
+        super(Integer.class);
+    }
+
+    @Override
+    public void serialize(Integer value, JsonGenerator gen, SerializerProvider provider) throws IOException {
+        gen.writeNumber(0);
+    }
+}
